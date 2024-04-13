f597641b77d4f3ba0c5165936f0271f52d6e8d48
Tatu Heinämäki <tatu.heinamaki@gmail.com>
2024-04-13 17:02:27 +0300

    Better human logic.

--- human/human.gd
+++ human/human.gd
@@ -11,38 +11,61 @@ const THINGS = ["human", "cat", "car"]
 var size = Vector2(0,0)
 
 # logic
+var can_have_action = true
 var current_action = null
+@onready var action_timer = $Action
+@onready var action_cooldown_timer = $ActionCooldown
 
 # movement logic
 @export var can_move = true
-var old_pos = Vector2(0,0)
+var screen_size
+
+var start_pos = Vector2.ZERO
+var idle_pos = Vector2.ZERO
+
+var old_pos = Vector2.ZERO
 var t = 0.0
 var goal = null
 
+
 func _ready():
 	size = Vector2($Sprite2D.texture.get_width(), $Sprite2D.texture.get_height()) * $Sprite2D.scale
-	print(position)
+	#print(position)
+	screen_size = get_viewport_rect().size - Vector2(0, 93 + size.y)
+	find_new_pos()
+	#print(idle_pos)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	t += delta * 0.04
-	if goal != null and can_move:
+	t += delta * 0.4
+	if can_move:
+		if goal != null:
 		#var new_pos = 
-		position = position.lerp(goal.global_position, t)
-		if position.distance_to(goal.global_position) < goal.size.x:
-			goal_reached()
+			position = position.lerp(goal.global_position, delta * 2)
+			if position.distance_to(goal.global_position) < goal.size.x:
+				goal_reached()
+		if goal == null and current_action == null:
+			#t += delta * 2
+			position = position.lerp(idle_pos, delta * 2)
+			if position.distance_to(idle_pos) < 1:
+				find_new_pos()
+				
+	#position = position.clamp(Vector2.ZERO, screen_size)
 
 
 func _on_vision_area_entered(area):
 	if area != self:
-		if THINGS.has(area.type):
+		if THINGS.has(area.type) and can_have_action:
+			print("i saw something")
 			goal = area
+			start_pos = global_position
 			#determine_action(area)
 
 
 func determine_action(action):
 	current_action = action
+	action_timer.start()
 	match current_action:
 		"human":
 			#old_pos = position
@@ -74,3 +97,24 @@ func change_type(new_type):
 		monitorable = false
 	type = new_type
 	monitorable = true
+
+
+func find_new_pos():
+	#t = 0
+	var movement_area = Vector2(randf_range(-size.x, size.x), randf_range(-size.y, size.y)) * 2
+	idle_pos = global_position + movement_area
+	idle_pos = idle_pos.clamp(Vector2.ZERO, screen_size)
+	start_pos = global_position
+	#print(global_position, position)
+	#print(idle_pos)
+
+
+func _on_action_cooldown_timeout():
+	can_have_action = true
+
+
+func _on_action_timeout():
+	can_have_action = false
+	goal = null
+	current_action = null
+	action_cooldown_timer.start()
